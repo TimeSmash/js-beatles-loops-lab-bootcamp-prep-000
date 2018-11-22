7df9111f277099d8334fb92aa2b19e141867c9f9
mcumm64 <mcumm64@gmail.com>
2018-11-22 22:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+function theBeatlesPlay (array1, array2) {
+  var emptyArray = [];
+  for (var counter = 0; counter < 5; counter++){
+  emptyArray.push(`${array1[counter]} plays ${array2[counter]}`);
+  }
+  if (emptyArray.length = 4) { 
+return emptyArray;
+  }
+}
