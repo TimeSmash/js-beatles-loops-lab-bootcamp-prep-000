3684dfd0a263c8e0bfdbef671d63eda8682082c9
mcumm64 <mcumm64@gmail.com>
2018-11-22 23:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay (array1, array2) {
   var emptyArray = [];
-  for (var counter = 0; counter < 5; counter++){
+  for (var counter = 0; counter < 4; counter++){
   emptyArray.push(`${array1[counter]} plays ${array2[counter]}`);
   }
   if (emptyArray.length = 4) { 
