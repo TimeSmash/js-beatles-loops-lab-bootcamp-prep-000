761274e9b240d45b6cf914ecff561a6b14c98b83
mcumm64 <mcumm64@gmail.com>
2018-11-22 23:18:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts (arrayOfFacts) {
 // need to return all strings in array with "!!!" at end 
 // Need to use while loop. If the array has all !!! at end of strings, then adding !!! should stop
 while (x < 5) {
- 
+ var x = 0;
 return arrayofFacts[x] + "!!!"
 x = x + 1;  }
 }
