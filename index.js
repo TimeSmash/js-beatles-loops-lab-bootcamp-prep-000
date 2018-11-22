bca786dc6ac174d2b9e3faee136d4def99acc38a
mcumm64 <mcumm64@gmail.com>
2018-11-22 22:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ return emptyArray;
 }
 
 const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and a boy scout", "He hated the sound of his own voice"];
-function johnLennonFacts (arrayOfFacts) 
+function johnLennonFacts (arrayOfFacts) { 
 // need to return all strings in array with "!!!" at end 
 // Need to use while loop. If the array has all !!! at end of strings, then adding !!! should stop
 while (arrayOfFacts.length < 5) {
